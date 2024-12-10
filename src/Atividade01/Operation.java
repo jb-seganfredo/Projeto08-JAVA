5959199df2d7339dc1c64ec2f704f7815548422c
Júlia Bueno Seganfredo <150600044+jb-seganfredo@users.noreply.github.com>
2024-12-09 21:24:05 -0300

    Solving exercise 1

---
+++ src/Atividade01/Operation.java
@@ -0,0 +1,48 @@
+package Atividade01;
+
+import java.util.Scanner;
+
+public class Operation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Digite o operador (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Erro: Divisão por zero!");
+                    scanner.close();
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operador inválido!");
+                scanner.close();
+                return;
+        }
+
+        System.out.printf("O resultado é: %.2f%n", result);
+        scanner.close();
+    }
+}
